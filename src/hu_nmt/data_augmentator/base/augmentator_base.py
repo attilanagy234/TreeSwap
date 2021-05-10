285bdbd637d6d292893db71b3f2ab399ab8f8000
Attila Nagy <attila.nagy@meltwater.com>
2021-05-10 17:44:45 +0200

    replace hyphens with underscores

--- src/hu_nmt/data_augmentator/base/augmentator_base.py
+++ src/hu_nmt/data_augmentator/base/augmentator_base.py
@@ -16,6 +16,6 @@ class AugmentatorBase(ABC):
 
     @staticmethod
     def reconstruct_sentence_from_node_ids(node_ids):
-        splitted_node_ids = [x.split('-') for x in node_ids]
+        splitted_node_ids = [x.split('_') for x in node_ids]
         splitted_node_ids = [(x[0], int(x[1])) for x in splitted_node_ids]
         return [y[0] for y in sorted(splitted_node_ids, key=itemgetter(1))]
