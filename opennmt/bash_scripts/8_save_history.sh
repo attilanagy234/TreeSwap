8c9db5cba7b1783017fd15d29f667e001eff6d4e
patrick-nanys <patrick.nanys2000@gmail.com>
2021-08-14 16:33:10 +0200

    Added .tsv style experiment tracking

--- opennmt/bash_scripts/8_save_history.sh
+++ opennmt/bash_scripts/8_save_history.sh
@@ -1,12 +1,20 @@
 HISTORY_DIR=../../history
-SAVE_DIR=$HISTORY_DIR/$(date '+%Y-%m-%d_%H:%M:%S')
+TSV_PATH=$HISTORY_DIR/history.tsv
+
+save_dir=$HISTORY_DIR/$(date '+%Y-%m-%d_%H:%M:%S')
+utils_path=$(grep 'utils_path:' config.yaml | awk '{ print $2 }')
 pair_output_path=$(grep 'pair_output_path:' config.yaml | awk '{ print $2 }')
 translate_model=$(grep 'translate_model:' config.yaml | awk '{ print $2 }')
 
-TO_SAVE="config.yaml run/final_result.txt run/pred.txt.sp run/pred.txt run/tensorboard $pair_output_path $translate_model"
+CONFIG_PATH="config.yaml"
+FINAL_RESULT_PATH="run/final_result.txt"
+TO_SAVE="$CONFIG_PATH $FINAL_RESULT_PATH run/pred.txt run/tensorboard $pair_output_path $translate_model"
+
 mkdir -p $HISTORY_DIR
-mkdir $SAVE_DIR
-for FILE in $TO_SAVE
+mkdir $save_dir
+for file in $TO_SAVE
 do
-    cp -r $FILE $SAVE_DIR/
+    cp -r $file $save_dir/
 done
+
+python3 $utils_path/save_history_to_tsv.py --yaml_path $CONFIG_PATH --result_path $FINAL_RESULT_PATH --history_path $save_dir --tsv_path $TSV_PATH
