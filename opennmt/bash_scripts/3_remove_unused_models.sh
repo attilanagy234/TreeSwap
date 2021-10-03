a5f0bb43546519336d7ce4145ae70b63dd4b624e
patrick-nanys <patrick.nanys2000@gmail.com>
2021-10-03 19:36:03 +0200

    Make script work with binary files

--- opennmt/bash_scripts/3_remove_unused_models.sh
+++ opennmt/bash_scripts/3_remove_unused_models.sh
@@ -1,7 +1,7 @@
 log_file=$(grep 'log_file' config.yaml | awk '{ print $2 }')
 
 echo "--Removing unused models--"
-best_model=run/$(grep -A1 'Model is improving' $log_file | tail -n1 | sed 's/.*\/\(.*_step_[0-9]\+\.pt\).*/\1/') && \
+best_model=run/$(grep --text -A1 'Model is improving' $log_file | tail -n1 | sed 's/.*\/\(.*_step_[0-9]\+\.pt\).*/\1/') && \
 echo "Best model found is: $best_model"
 mv $best_model run/model && \
 rm run/*.pt && \
