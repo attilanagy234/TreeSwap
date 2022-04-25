33b0475d2ae034f76c8c2c8caeb698292a14c273
dorinapetra <dorinapetra@gmail.com>
2022-04-25 21:03:55 +0200

    computing trees paralell

--- src/hu_nmt/data_augmentator/entrypoints/precompute_paralell_dependency_trees.py
+++ src/hu_nmt/data_augmentator/entrypoints/precompute_paralell_dependency_trees.py
@@ -1,8 +1,7 @@
+import os
+import click
 from typing import Iterator
 
-import click
-
-from hu_nmt.data_augmentator.base.depedency_parser_base import DependencyParserBase
 from hu_nmt.data_augmentator.dependency_parsers.spacy_dependency_parser import SpacyDependencyParser
 from hu_nmt.data_augmentator.utils.logger import get_logger
 from hu_nmt.data_augmentator.dependency_parsers.english_dependency_parser import EnglishDependencyParser
@@ -22,27 +21,28 @@ def main(src_tag, tgt_tag, src_input_path, tgt_input_path, dep_tree_output_path,
         'en': EnglishDependencyParser(),
         'hu': SpacyDependencyParser(lang='hu')
     }
-    src_dep_parser: DependencyParserBase = dep_parsers[src_tag]
+    src_dep_parser = dep_parsers[src_tag]
     tgt_dep_parser = dep_parsers[tgt_tag]
 
-    src_sentence_generator: Iterator = None
-    tgt_sentence_generator: Iterator = None
+    src_sentence_generator: Iterator = src_dep_parser._get_file_line_generator(src_input_path)
+    tgt_sentence_generator: Iterator = tgt_dep_parser._get_file_line_generator(tgt_input_path)
 
-    for src_batch, tgt_batch in zip(src_sentence_generator, tgt_sentence_generator):
+    for file_idx, (src_batch, tgt_batch) in enumerate(zip(src_sentence_generator, tgt_sentence_generator)):
+        src_list_of_dep_rel_lists = []
+        tgt_list_of_dep_rel_lists = []
         for src_sent, tgt_sent in zip(src_batch, tgt_batch):
-            pass
+            src_sents = src_dep_parser.sentence_to_node_relationship_list(src_dep_parser.nlp_pipeline, src_sent)
+            tgt_sents = tgt_dep_parser.sentence_to_node_relationship_list(src_dep_parser.nlp_pipeline, tgt_sent)
 
+            if src_sents == [] or tgt_sents == []:
+                continue
+            src_list_of_dep_rel_lists.append(src_sents)
+            tgt_list_of_dep_rel_lists.append(tgt_sents)
 
-
-
-    eng_dep_parser = EnglishDependencyParser()
-    eng_dep_parser.file_to_serialized_dep_graph_files(data_input_path, dep_tree_output_path, int(file_batch_size))
+        src_dep_parser.write_dep_graphs_to_file(os.path.join(dep_tree_output_path, src_tag), file_idx, src_list_of_dep_rel_lists)
+        tgt_dep_parser.write_dep_graphs_to_file(os.path.join(dep_tree_output_path, tgt_tag), file_idx, tgt_list_of_dep_rel_lists)
 
 
 if __name__ == '__main__':
     main()
 
-    # During local testing
-    # $1 /Users/attilanagy/Personal/hu-nmt/src/hu_nmt/data_augmentator/data/augmentation_input_data/train_200000_subsample.en
-    # $2 /Users/attilanagy/Personal/hu-nmt/src/hu_nmt/data_augmentator/data/eng/parsed_sentences
-    # $3 10000
