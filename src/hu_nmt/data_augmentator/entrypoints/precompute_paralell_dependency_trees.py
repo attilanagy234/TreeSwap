850bfab1b6976670a22cc80d421ef668ff44adcf
dorinapetra <dorinapetra@gmail.com>
2022-05-05 11:57:16 +0200

    add DependencyParserFactory

--- src/hu_nmt/data_augmentator/entrypoints/precompute_paralell_dependency_trees.py
+++ src/hu_nmt/data_augmentator/entrypoints/precompute_paralell_dependency_trees.py
@@ -2,27 +2,22 @@ import os
 import click
 from typing import Iterator
 
-from hu_nmt.data_augmentator.dependency_parsers.spacy_dependency_parser import SpacyDependencyParser
+from hu_nmt.data_augmentator.dependency_parsers.dependency_parser_factory import DependencyParserFactory
 from hu_nmt.data_augmentator.utils.logger import get_logger
-from hu_nmt.data_augmentator.dependency_parsers.english_dependency_parser import EnglishDependencyParser
 
 log = get_logger(__name__)
 
 
 @click.command()
-@click.argument('src_tag')
-@click.argument('tgt_tag')
+@click.argument('src_lang_code')
+@click.argument('tgt_lang_code')
 @click.argument('src_input_path')
 @click.argument('tgt_input_path')
 @click.argument('dep_tree_output_path')
 @click.argument('file_batch_size')
-def main(src_tag, tgt_tag, src_input_path, tgt_input_path, dep_tree_output_path, file_batch_size):
-    dep_parsers = {
-        'en': EnglishDependencyParser(),
-        'hu': SpacyDependencyParser(lang='hu')
-    }
-    src_dep_parser = dep_parsers[src_tag]
-    tgt_dep_parser = dep_parsers[tgt_tag]
+def main(src_lang_code, tgt_lang_code, src_input_path, tgt_input_path, dep_tree_output_path, file_batch_size):
+    src_dep_parser = DependencyParserFactory.get_dependency_parser(src_lang_code)
+    tgt_dep_parser = DependencyParserFactory.get_dependency_parser(tgt_lang_code)
 
     src_sentence_generator: Iterator = src_dep_parser._get_file_line_generator(src_input_path)
     tgt_sentence_generator: Iterator = tgt_dep_parser._get_file_line_generator(tgt_input_path)
@@ -48,8 +43,8 @@ def main(src_tag, tgt_tag, src_input_path, tgt_input_path, dep_tree_output_path,
             src_list_of_dep_rel_lists.append(src_sents)
             tgt_list_of_dep_rel_lists.append(tgt_sents)
 
-        src_dep_parser.write_dep_graphs_to_file(os.path.join(dep_tree_output_path, src_tag), 1, src_list_of_dep_rel_lists)
-        tgt_dep_parser.write_dep_graphs_to_file(os.path.join(dep_tree_output_path, tgt_tag), 1, tgt_list_of_dep_rel_lists)
+        src_dep_parser.write_dep_graphs_to_file(os.path.join(dep_tree_output_path, src_lang_code), 1, src_list_of_dep_rel_lists)
+        tgt_dep_parser.write_dep_graphs_to_file(os.path.join(dep_tree_output_path, tgt_lang_code), 1, tgt_list_of_dep_rel_lists)
 
 
 if __name__ == '__main__':
