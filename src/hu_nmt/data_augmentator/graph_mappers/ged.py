2cf587103c96cdb15f777c4383f8305f77027efb
dorinapetra <dorinapetra@gmail.com>
2022-06-02 10:16:54 +0200

    add lang codes to ged

--- src/hu_nmt/data_augmentator/graph_mappers/ged.py
+++ src/hu_nmt/data_augmentator/graph_mappers/ged.py
@@ -1,13 +1,33 @@
 from networkx import graph_edit_distance
 
+from hu_nmt.data_augmentator.dependency_parsers.dependency_parser_factory import DependencyParserFactory
+
 
 class GED:
-    def __init__(self, node_cost=1, edge_cost=1, node_subt=2, edge_subt=2, timeout=10):
+    _src_dep_parser = None
+    _tgt_dep_parser = None
+
+    def __init__(self, src_lang_code, tgt_lang_code, node_cost=1, edge_cost=1, node_subt=2, edge_subt=2, timeout=10):
+        self.src_lang_code = src_lang_code
+        self.tgt_lang_code = tgt_lang_code
+
         self.timeout = timeout
         self.node_subt = node_subt
         self.edge_subt = edge_subt
         self.edge_cost = edge_cost
         self.node_cost = node_cost
+
+    @property
+    def src_dep_parser(self):
+        if not self._src_dep_parser:
+            self._src_dep_parser = DependencyParserFactory.get_dependency_parser(self.src_lang_code)
+        return self._src_dep_parser
+
+    @property
+    def tgt_dep_parser(self):
+        if not self._tgt_dep_parser:
+            self._tgt_dep_parser = DependencyParserFactory.get_dependency_parser(self.tgt_lang_code)
+        return self._tgt_dep_parser
 
     def _node_match(self, n1, n2):
         return n1['postag'] == n2['postag']
@@ -51,4 +71,11 @@ class GED:
         dist = self.get_ged(graph1, graph2)
         max_dist = len(graph1.nodes) * 2 - 2 + 2 * len(graph2.nodes) - 2
         return float(max_dist - dist) / float(max_dist)
+
+    def get_normalized_distance_from_sentences(self, src_sent, tgt_sent):
+        src_graph = self.src_dep_parser.sentence_to_dep_parse_tree(src_sent)
+        tgt_graph = self.tgt_dep_parser.sentence_to_dep_parse_tree(tgt_sent)
+
+        return self.get_normalized_distance(src_graph, tgt_graph)
+
 
