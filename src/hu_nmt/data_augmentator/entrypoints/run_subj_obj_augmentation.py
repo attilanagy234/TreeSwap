361f94ac92a8e7c45513afdb202c4b3c404e2f40
dorinapetra <dorinapetra@gmail.com>
2022-06-09 14:25:41 +0200

    modify augmentation entrypoint to graph based augmentation

--- src/hu_nmt/data_augmentator/entrypoints/run_subj_obj_augmentation.py
+++ src/hu_nmt/data_augmentator/entrypoints/run_subj_obj_augmentation.py
@@ -1,5 +1,7 @@
 import click
 from tqdm import tqdm
+
+from hu_nmt.data_augmentator.augmentators.graph_based_augmentator import GraphBasedAugmentator
 from hu_nmt.data_augmentator.augmentators.subject_object_augmentator import SubjectObjectAugmentator
 from hu_nmt.data_augmentator.dependency_parsers.stanza_dependency_parser import StanzaDependencyParser
 from hu_nmt.data_augmentator.dependency_parsers.spacy_dependency_parser import SpacyDependencyParser
@@ -18,7 +20,8 @@ log = get_logger(__name__)
 @click.argument('augmentation_output_path')
 @click.argument('augmented_data_ratio')
 @click.option('--use_filters', is_flag=True, default=False, help='Use filters after the augmentation')
-@click.option('--filter_quantile', default=0.0, help='Quantile to use when filtering with cosine similarity. (Throw away anything below this quantile.)')
+@click.option('--filter_quantile', default=0.0,
+              help='Quantile to use when filtering with cosine similarity. (Throw away anything below this quantile.)')
 @click.option('--src_model_path', default='', help='Path to model to translate the source sentences with')
 @click.option('--tgt_model_path', default='', help='Path to model to translate the target sentences with')
 @click.option('--sp_model_path', default='', help='Sentencepiece model path')
@@ -26,10 +29,11 @@ log = get_logger(__name__)
 @click.option('--output_format', default='basic', help='Supported output formats: basic (default), tsv')
 @click.option('--save_original/--dont_save_original', default=False)
 @click.option('--separate_augmentation', default=False)
-
-def main(src_language, tgt_language, src_data_folder, tgt_data_folder, augmentation_output_path,
-         augmented_data_ratio, use_filters, filter_quantile, src_model_path, tgt_model_path, sp_model_path,
-         filter_batch_size, output_format, save_original, separate_augmentation):
+@click.option('--augmentation_type', default='base')
+@click.option('--treshold', default=0.5)
+def main(src_language, tgt_language, src_data_folder, tgt_data_folder, augmentation_output_path, augmented_data_ratio,
+         use_filters, filter_quantile, src_model_path, tgt_model_path, sp_model_path, filter_batch_size, output_format,
+         save_original, separate_augmentation, augmentation_type, threshold):
     dep_parsers = {
         'en': StanzaDependencyParser(lang='en'),
         'hu': SpacyDependencyParser(lang='hu')
@@ -42,11 +46,19 @@ def main(src_language, tgt_language, src_data_folder, tgt_data_folder, augmentat
 
     filters = []
     if use_filters:
-        filters.append(BleuFilter(filter_quantile, src_model_path, tgt_model_path, sp_model_path, tgt_language, filter_batch_size))
-    augmentator = SubjectObjectAugmentator(None, None, augmented_data_ratio, random_seed=15, filters=filters,
-                                           output_path=augmentation_output_path, output_format=output_format,
-                                           save_original=save_original, separate_augmentation=separate_augmentation)
+        filters.append(
+            BleuFilter(filter_quantile, src_model_path, tgt_model_path, sp_model_path, tgt_language, filter_batch_size))
 
+    if augmentation_type == 'ged' or augmentation_type == 'edge_mapper':
+        augmentator = GraphBasedAugmentator(src_language, tgt_language, threshold, None, None, augmented_data_ratio,
+                                            random_seed=15, filters=filters, output_path=augmentation_output_path,
+                                            output_format=output_format, save_original=save_original,
+                                            separate_augmentation=separate_augmentation,
+                                            similarity_type=augmentation_type)
+    else:
+        augmentator = SubjectObjectAugmentator(None, None, augmented_data_ratio, random_seed=15, filters=filters,
+                                               output_path=augmentation_output_path, output_format=output_format,
+                                               save_original=save_original, separate_augmentation=separate_augmentation)
     log.info('Reading parsed dependency trees')
     graph_cnt = 0
     with tqdm() as pbar:
@@ -75,4 +87,4 @@ if __name__ == '__main__':
     # $4 15
 
     # During hu-nmt use
-    # /home1/hu-nmt/data/Hunglish2/augmented/dep_trees/en /home1/hu-nmt/data/Hunglish2/augmented/dep_trees/hu /home1/hu-nmt/data/Hunglish2/augmented/generated-0.25 0.25
+    #  /home1/hu-nmt/data/Hunglish2/augmented/dep_trees/en /home1/hu-nmt/data/Hunglish2/augmented/dep_trees/hu /home1/hu-nmt/data/Hunglish2/augmented/generated-0.25 0.25
