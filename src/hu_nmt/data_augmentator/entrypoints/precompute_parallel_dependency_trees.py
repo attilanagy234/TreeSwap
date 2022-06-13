92d251a944d000699ff26e402c5c24b5b51d240f
patrick-nanys <patrick.nanys@meltwater.com>
2022-06-13 09:16:43 +0200

    Make precompute_parallel_dependency_trees entrypoint use multiprocessing

--- src/hu_nmt/data_augmentator/entrypoints/precompute_parallel_dependency_trees.py
+++ src/hu_nmt/data_augmentator/entrypoints/precompute_parallel_dependency_trees.py
@@ -22,36 +22,39 @@ def main(src_lang_code, tgt_lang_code, src_input_path, tgt_input_path, dep_tree_
     src_sentence_generator: Iterator = src_dep_parser.get_file_line_generator(src_input_path)
     tgt_sentence_generator: Iterator = tgt_dep_parser.get_file_line_generator(tgt_input_path)
 
-    have_more_sentences_to_process = True
-    file_idx = 1
-    while have_more_sentences_to_process:
-        src_batch_of_sentences = []
-        tgt_batch_of_sentences = []
-        try:
-            for _ in range(int(file_batch_size)):
-                src_batch_of_sentences.append(next(src_sentence_generator))
-                tgt_batch_of_sentences.append(next(tgt_sentence_generator))
-        except StopIteration:
-            have_more_sentences_to_process = False
+    src_dep_parser.sentences_to_serialized_dep_graph_files(src_sentence_generator, os.path.join(dep_tree_output_path, src_lang_code), file_batch_size)
+    tgt_dep_parser.sentences_to_serialized_dep_graph_files(tgt_sentence_generator, os.path.join(dep_tree_output_path, tgt_lang_code), file_batch_size)
 
-        src_list_of_dep_rel_lists = []
-        tgt_list_of_dep_rel_lists = []
-        for src_sent, tgt_sent in zip(src_batch_of_sentences, tgt_batch_of_sentences):
-            try:
-                src_sents = src_dep_parser.sentence_to_node_relationship_list(src_dep_parser.nlp_pipeline, src_sent)
-                tgt_sents = tgt_dep_parser.sentence_to_node_relationship_list(tgt_dep_parser.nlp_pipeline, tgt_sent)
-            except:
-                log.exception(f'Could not compute dependency tree for one of the sentences: src: {src_sent}, tgt: {tgt_sent}')
-                continue
-            if src_sents == [] or tgt_sents == []:
-                continue
-            src_list_of_dep_rel_lists.append(src_sents)
-            tgt_list_of_dep_rel_lists.append(tgt_sents)
-
-        src_dep_parser.write_dep_graphs_to_file(os.path.join(dep_tree_output_path, src_lang_code), file_idx, src_list_of_dep_rel_lists)
-        tgt_dep_parser.write_dep_graphs_to_file(os.path.join(dep_tree_output_path, tgt_lang_code), file_idx, tgt_list_of_dep_rel_lists)
-
-        file_idx += 1
+    # have_more_sentences_to_process = True
+    # file_idx = 1
+    # while have_more_sentences_to_process:
+    #     src_batch_of_sentences = []
+    #     tgt_batch_of_sentences = []
+    #     try:
+    #         for _ in range(int(file_batch_size)):
+    #             src_batch_of_sentences.append(next(src_sentence_generator))
+    #             tgt_batch_of_sentences.append(next(tgt_sentence_generator))
+    #     except StopIteration:
+    #         have_more_sentences_to_process = False
+    #
+    #     src_list_of_dep_rel_lists = []
+    #     tgt_list_of_dep_rel_lists = []
+    #     for src_sent, tgt_sent in zip(src_batch_of_sentences, tgt_batch_of_sentences):
+    #         try:
+    #             src_sents = src_dep_parser.sentence_to_node_relationship_list(src_dep_parser.nlp_pipeline, src_sent)
+    #             tgt_sents = tgt_dep_parser.sentence_to_node_relationship_list(tgt_dep_parser.nlp_pipeline, tgt_sent)
+    #         except:
+    #             log.exception(f'Could not compute dependency tree for one of the sentences: src: {src_sent}, tgt: {tgt_sent}')
+    #             continue
+    #         if src_sents == [] or tgt_sents == []:
+    #             continue
+    #         src_list_of_dep_rel_lists.append(src_sents)
+    #         tgt_list_of_dep_rel_lists.append(tgt_sents)
+    #
+    #     src_dep_parser.write_dep_graphs_to_file(os.path.join(dep_tree_output_path, src_lang_code), file_idx, src_list_of_dep_rel_lists)
+    #     tgt_dep_parser.write_dep_graphs_to_file(os.path.join(dep_tree_output_path, tgt_lang_code), file_idx, tgt_list_of_dep_rel_lists)
+    #
+    #     file_idx += 1
 
 
 if __name__ == '__main__':
