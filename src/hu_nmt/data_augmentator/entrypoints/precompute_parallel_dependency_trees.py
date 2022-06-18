c8afff3eb33b0f24aed63a27b2daaf41a5691aca
patrick-nanys <patrick.nanys@meltwater.com>
2022-06-18 15:09:16 +0200

    Remove commented code

--- src/hu_nmt/data_augmentator/entrypoints/precompute_parallel_dependency_trees.py
+++ src/hu_nmt/data_augmentator/entrypoints/precompute_parallel_dependency_trees.py
@@ -25,37 +25,6 @@ def main(src_lang_code, tgt_lang_code, src_input_path, tgt_input_path, dep_tree_
     src_dep_parser.sentences_to_serialized_dep_graph_files(src_sentence_generator, os.path.join(dep_tree_output_path, src_lang_code), int(file_batch_size))
     tgt_dep_parser.sentences_to_serialized_dep_graph_files(tgt_sentence_generator, os.path.join(dep_tree_output_path, tgt_lang_code), int(file_batch_size))
 
-    # have_more_sentences_to_process = True
-    # file_idx = 1
-    # while have_more_sentences_to_process:
-    #     src_batch_of_sentences = []
-    #     tgt_batch_of_sentences = []
-    #     try:
-    #         for _ in range(int(file_batch_size)):
-    #             src_batch_of_sentences.append(next(src_sentence_generator))
-    #             tgt_batch_of_sentences.append(next(tgt_sentence_generator))
-    #     except StopIteration:
-    #         have_more_sentences_to_process = False
-    #
-    #     src_list_of_dep_rel_lists = []
-    #     tgt_list_of_dep_rel_lists = []
-    #     for src_sent, tgt_sent in zip(src_batch_of_sentences, tgt_batch_of_sentences):
-    #         try:
-    #             src_sents = src_dep_parser.sentence_to_node_relationship_list(src_dep_parser.nlp_pipeline, src_sent)
-    #             tgt_sents = tgt_dep_parser.sentence_to_node_relationship_list(tgt_dep_parser.nlp_pipeline, tgt_sent)
-    #         except:
-    #             log.exception(f'Could not compute dependency tree for one of the sentences: src: {src_sent}, tgt: {tgt_sent}')
-    #             continue
-    #         if src_sents == [] or tgt_sents == []:
-    #             continue
-    #         src_list_of_dep_rel_lists.append(src_sents)
-    #         tgt_list_of_dep_rel_lists.append(tgt_sents)
-    #
-    #     src_dep_parser.write_dep_graphs_to_file(os.path.join(dep_tree_output_path, src_lang_code), file_idx, src_list_of_dep_rel_lists)
-    #     tgt_dep_parser.write_dep_graphs_to_file(os.path.join(dep_tree_output_path, tgt_lang_code), file_idx, tgt_list_of_dep_rel_lists)
-    #
-    #     file_idx += 1
-
 
 if __name__ == '__main__':
     main()
