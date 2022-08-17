c2b19953bd65f50f4c7332c3191cb61b9096bfec
dorinapetra <dorinapetra@gmail.com>
2022-08-17 13:25:47 +0200

    fix using DependencyGraphWrapper

--- src/hu_nmt/data_augmentator/graph_mappers/graph_similarity_base.py
+++ src/hu_nmt/data_augmentator/graph_mappers/graph_similarity_base.py
@@ -1,14 +1,14 @@
 from abc import ABC, abstractmethod
-
-import networkx as nx
 from typing import Tuple, Dict
+
+from hu_nmt.data_augmentator.wrapper.dependency_graph_wrapper import DependencyGraphWrapper
 
 Edge = Tuple[str, str, Dict]
 Node = str
 
+
 class GraphSimilarityBase(ABC):
 
     @abstractmethod
-    def get_similarity_from_graphs(self, src_graph: nx.DiGraph, tgt_graph: nx.DiGraph) -> float:
+    def get_similarity_from_graphs(self, src_graph: DependencyGraphWrapper, tgt_graph: DependencyGraphWrapper) -> float:
         raise NotImplementedError
-
